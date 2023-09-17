9ec089a6e37be6ecdeec038c23b8604e4b79c886
Sachira Heshan <sachiraheshan99@gmail.com>
2023-09-17 19:11:19 +0530

    Navbar completed

--- frontend/src/pages/user/HomePage.js
+++ frontend/src/pages/user/HomePage.js
@@ -1,17 +1,33 @@
 import React from 'react'
 import "./HomePage.css"
-import { LuSearch } from 'react-icons/lu'
-import { MdArrowForward } from 'react-icons/md'
 
 function HomePage() {
   return (
     <div>
       <div className='home-page'>
-        <h1 className='hero-text'><span className='block-text'>Precision Nets for Precision Fishing</span></h1>
-        <p className='hero-para'>Crafting Success, One Net at a Time<span className='new-line'> Elevate Your Fishing Experience with Our Premium Nets!</span></p>
+        <div className='home-hero'>
+          <h1 className='hero-text'><span className='block-text'>Precision Nets for Precision Fishing</span></h1>
+          <p className='hero-para'>Crafting Success, One Net at a Time<span className='new-line'> Elevate Your Fishing Experience with Our Premium Nets!</span></p>
 
-        <button className='explore-button' onClick={() => {  window.location.href = '/planets'} }>
-          <span className='button-text'>Explore</span><MdArrowForward /></button>
+        </div>
+        <div className="our-story">
+          <h2 className='section-title'>Our Story</h2>
+          <p>"From humble beginnings on the shores of North Sea, our company embarked on a journey in [Year]. Over the decades, we've evolved into a trusted name in fishing nets. Our dedication to craftsmanship and innovation has driven us to create nets that withstand the rigors of the sea, empowering fishermen worldwide. Join us as we share our story of passion, growth, and commitment to providing the finest fishing nets."</p>
+          <p>"From humble beginnings on the shores of North Sea, our company embarked on a journey in [Year]. Over the decades, we've evolved into a trusted name in fishing nets. Our dedication to craftsmanship and innovation has driven us to create nets that withstand the rigors of the sea, empowering fishermen worldwide. Join us as we share our story of passion, growth, and commitment to providing the finest fishing nets."</p>
+          <p>"From humble beginnings on the shores of North Sea, our company embarked on a journey in [Year]. Over the decades, we've evolved into a trusted name in fishing nets. Our dedication to craftsmanship and innovation has driven us to create nets that withstand the rigors of the sea, empowering fishermen worldwide. Join us as we share our story of passion, growth, and commitment to providing the finest fishing nets."</p>
+          <p>"From humble beginnings on the shores of North Sea, our company embarked on a journey in [Year]. Over the decades, we've evolved into a trusted name in fishing nets. Our dedication to craftsmanship and innovation has driven us to create nets that withstand the rigors of the sea, empowering fishermen worldwide. Join us as we share our story of passion, growth, and commitment to providing the finest fishing nets."</p>
+          <p>"From humble beginnings on the shores of North Sea, our company embarked on a journey in [Year]. Over the decades, we've evolved into a trusted name in fishing nets. Our dedication to craftsmanship and innovation has driven us to create nets that withstand the rigors of the sea, empowering fishermen worldwide. Join us as we share our story of passion, growth, and commitment to providing the finest fishing nets."</p>
+          <p>"From humble beginnings on the shores of North Sea, our company embarked on a journey in [Year]. Over the decades, we've evolved into a trusted name in fishing nets. Our dedication to craftsmanship and innovation has driven us to create nets that withstand the rigors of the sea, empowering fishermen worldwide. Join us as we share our story of passion, growth, and commitment to providing the finest fishing nets."</p>
+        </div>
+        <div className='discover-us'>
+          <h2 className='section-title'>Discover Us</h2>
+        </div>
+        <div className='our-products'>
+          <h2 className='section-title'>Our Products</h2>
+        </div>
+        <div className='featured-articles'>
+          <h2 className='section-title'>Featured Articles</h2>
+        </div>
       </div>
     </div>
   )
