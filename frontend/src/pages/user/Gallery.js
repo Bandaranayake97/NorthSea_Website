1646dea5794819a211fca31b6153bae7043d89a2
Ruvindya <2019e117@eng.jfn.ac.lk>
2023-09-28 20:54:52 +0530

    Gallery and joinUs completed

--- frontend/src/pages/user/Gallery.js
+++ frontend/src/pages/user/Gallery.js
@@ -1,14 +1,8 @@
 import React from 'react'
 import './Gallery.css'
+import data from "../../data/index.json";
 
-import article_image_01 from '../../images/article-01.png'
-import article_image_02 from '../../images/article-02.png'
-import article_image_03 from '../../images/article-03.png'
 
-import place_image_01 from '../../images/place-01.png'
-import place_image_02 from '../../images/place-02.png'
-import place_image_03 from '../../images/place-03.png'
-import place_image_04 from '../../images/place-03.png'
 
 function Gallery() {
     return (
@@ -24,152 +18,25 @@ function Gallery() {
                     <div className='gallery--section--row--container'>
 
                         <div className='gallery--section--row'>
-                                <div className='gallery--section--card'>
+                        {data?.gallery?.map((item, index) => (
+                                    <div key={index} className='gallery--section--card'>
                                         <div className="gallery--section--img">
-                                            <img src={article_image_01} alt="Placeholder" />
+                                            <img src={item.src} alt="Placeholder" />
                                         </div>                       
                                         <div className='gallery--section--card--content'>
                                             <div>
-                                                <h3 className="gallery--section--title">Image Title</h3>
-                                                <p className="gallery--section--description">In this Section there will be a small description about the pitcher</p>
-                                                <p className="gallery--section--description">Date: 13/12/2022</p>
+                                                <h3 className="gallery--section--title">{item.title}</h3>
+                                                <p className="gallery--section--description">{item.description}</p>
+                                                <p className="gallery--section--description">{item.date}</p>
                                             </div>
-                                        </div>
-                                        
+                                        </div>     
                                     </div>
-
-                                    <div className='gallery--section--card'>
-                                        <div className="gallery--section--img">
-                                            <img src={place_image_02} alt="Placeholder" />
-                                        </div>                       
-                                        <div className='gallery--section--card--content'>
-                                            <div>
-                                                <h3 className="gallery--section--title">Image Title</h3>
-                                                <p className="gallery--section--description">Image small description</p>
-                                                <p className="gallery--section--description">Date: 13/12/2022</p>
-                                            </div>
-                                        </div>
-                                        
-                                    </div>
-
-                                    <div className='gallery--section--card'>
-                                        <div className="gallery--section--img">
-                                            <img src={article_image_03} alt="Placeholder" />
-                                        </div>                       
-                                        <div className='gallery--section--card--content'>
-                                            <div>
-                                                <h3 className="gallery--section--title">Image Title</h3>
-                                                <p className="gallery--section--description">Image small description</p>
-                                                <p className="gallery--section--description">Date: 13/12/2022</p>
-                                            </div>
-                                        </div>
-                                        
-                                    </div>
-
+                                ))}
                         </div>
-{/*Second Row */}
-
-<div className='gallery--section--row'>
-                                <div className='gallery--section--card'>
-                                        <div className="gallery--section--img">
-                                            <img src={place_image_04} alt="Placeholder" />
-                                        </div>                       
-                                        <div className='gallery--section--card--content'>
-                                            <div>
-                                                <h3 className="gallery--section--title">Image Title</h3>
-                                                <p className="gallery--section--description">Image small description</p>
-                                                <p className="gallery--section--description">Date: 13/12/2022</p>
-                                            </div>
-                                        </div>
-                                        
-                                    </div>
-
-                                    <div className='gallery--section--card'>
-                                        <div className="gallery--section--img">
-                                            <img src={article_image_01} alt="Placeholder" />
-                                        </div>                       
-                                        <div className='gallery--section--card--content'>
-                                            <div>
-                                                <h3 className="gallery--section--title">Image Title</h3>
-                                                <p className="gallery--section--description">Image small description</p>
-                                                <p className="gallery--section--description">Date: 13/12/2022</p>
-                                            </div>
-                                        </div>
-                                        
-                                    </div>
-
-                                    <div className='gallery--section--card'>
-                                        <div className="gallery--section--img">
-                                            <img src={article_image_02} alt="Placeholder" />
-                                        </div>                       
-                                        <div className='gallery--section--card--content'>
-                                            <div>
-                                                <h3 className="gallery--section--title">Image Title</h3>
-                                                <p className="gallery--section--description">Image small description</p>
-                                                <p className="gallery--section--description">Date: 13/12/2022</p>
-                                            </div>
-                                        </div>
-                                        
-                                    </div>
-
-                        </div>
-{/* Third ROW */}
-                        <div className='gallery--section--row'>
-                                <div className='gallery--section--card'>
-                                        <div className="gallery--section--img">
-                                            <img src={place_image_01} alt="Placeholder" />
-                                        </div>                       
-                                        <div className='gallery--section--card--content'>
-                                            <div>
-                                                <h3 className="gallery--section--title">Image Title</h3>
-                                                <p className="gallery--section--description">Image small description</p>
-                                                <p className="gallery--section--description">Date: 13/12/2022</p>
-                                            </div>
-                                        </div>
-                                        
-                                    </div>
-
-                                    <div className='gallery--section--card'>
-                                        <div className="gallery--section--img">
-                                            <img src={place_image_02} alt="Placeholder" />
-                                        </div>                       
-                                        <div className='gallery--section--card--content'>
-                                            <div>
-                                                <h3 className="gallery--section--title">Image Title</h3>
-                                                <p className="gallery--section--description">Image small description</p>
-                                                <p className="gallery--section--description">Date: 13/12/2022</p>
-                                            </div>
-                                        </div>
-                                        
-                                    </div>
-
-                                    <div className='gallery--section--card'>
-                                        <div className="gallery--section--img">
-                                            <img src={place_image_03} alt="Placeholder" />
-                                        </div>                       
-                                        <div className='gallery--section--card--content'>
-                                            <div>
-                                                <h3 className="gallery--section--title">Image Title</h3>
-                                                <p className="gallery--section--description">Image small description</p>
-                                                <p className="gallery--section--description">Date: 13/12/2022</p>
-                                            </div>
-                                        </div>
-                                        
-                                    </div>
-
-                        </div>
-                            
-
-                    </div>
-                    
-                    
-
+                    </div>                   
                 </div>
-
             </div>
-
-        </div>
-        
+        </div>       
     );
 }
 
