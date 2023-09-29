8f785e9216b803507ae34b2e956e9d1d1bae02cd
AnukaMithara <anukamithara@gmail.com>
2023-09-29 13:24:45 +0530

    Finalized Announcement Page

--- frontend/src/pages/user/Announcements.js
+++ frontend/src/pages/user/Announcements.js
@@ -1,63 +1,134 @@
-import React from 'react'
-import './Announcements.css'
+import React from "react";
+import "./Announcements.css";
+import { BsFillBookmarkFill } from "react-icons/bs";
+import { GrAnnounce } from "react-icons/gr";
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
+import announcementImage from "../../images/hiring.jpeg";
 
 function Announcements() {
-
-    return (
-
-        <div className='announcement-page'>
-
-            <div className='home-hero'>
-                <div className='section-content-container'>
-                    <h1 className='hero-text'><span className='block-text'>Precision Nets for Precision Fishing</span></h1>
-                </div>
-            </div>
-
-
-            <div className="tender-container">
-            <h2 className='section-title'>Tenders</h2>
-                <ul className="tender-list">
-                    {/* Each tender is a list item */}
-                    <li><span className="bullet-icon">📌</span> Tender 1</li>
-                    <li><span className="bullet-icon">📌</span> Tender 2</li>
-                    <li><span className="bullet-icon">📌</span> Tender 3</li>
-                    {/* Add more tender items as needed */}
-                </ul>
-            </div>
-
-            {/* Announcements Section */}
-            <div className="announcement-container">
-                <h2 className='section-title'>Announcements</h2>
-                <div className="announcement-card">
-                    {/* Each announcement is a card */}
-                    <div className="announcement-image">
-                        <img src="announcement-image-1.jpg" alt="Announcement 1" />
-                    </div>
-                    <div className="announcement-content">
-                        <h3>Announcement 1</h3>
-                        <p>Content for Announcement 1 goes here.</p>
-                    </div>
-                </div>
-
-                <div className="announcement-card">
-                    {/* Another announcement card */}
-                    <div className="announcement-image">
-                        <img src="announcement-image-2.jpg" alt="Announcement 2" />
-                    </div>
-                    <div className="announcement-content">
-                        <h3>Announcement 2</h3>
-                        <p>Content for Announcement 2 goes here.</p>
-                    </div>
-                </div>
-                {/* Add more announcement cards as needed */}
-            </div>
-
+  return (
+    <div className="announcement-page">
+      <div className="home-hero">
+        <div className="section-content-container">
+          <h1 className="hero-text">
+            <span className="block-text">Announcements</span>
+          </h1>
         </div>
-    )
+      </div>
+
+
+      {/*   <h2 className="noTenders">No tenders or announcements available 
+            at the moment. Keep in touch !</h2>
+       */}
+
+
+      <h2 className="section-title">Tenders</h2>
+      <div className="tender-container">
+        <ul className="tender-list">
+          <p className="tender-item">
+            <span className="bookmark-icon">
+              <BsFillBookmarkFill size={25} />
+            </span>
+            About the tender detail should include here. About the tender detail
+            should include here. About the tender detail should include here.
+            About the tender detail should include here.
+          </p>
+
+          <p className="tender-item">
+            <span className="bookmark-icon">
+              <BsFillBookmarkFill size={25} />
+            </span>
+            About the tender detail should include here. About the tender detail
+            should include here. About the tender detail should include here.
+            About the tender detail should include here.
+          </p>
+
+          <p className="tender-item">
+            <span className="bookmark-icon">
+              <BsFillBookmarkFill size={25} />
+            </span>
+            About the tender detail should include here. About the tender detail
+            should include here. About the tender detail should include here.
+            About the tender detail should include here.
+          </p>
+          <p className="tender-item">
+            <span className="bookmark-icon">
+              <BsFillBookmarkFill size={25} />
+            </span>
+            About the tender detail should include here. About the tender detail
+            should include here. About the tender detail should include here.
+            About the tender detail should include here.
+          </p>
+
+          {/* Add more tender items as needed */}
+        </ul>
+      </div>
+
+      {/* Announcements Section */}
+
+      <h2 className="section-title">
+        <span className="announce-icon">
+          <GrAnnounce size={40} />
+        </span>
+        Announcements
+      </h2>
+      <div className="announcement-container">
+        <div className="announcement-card">
+          <img
+            src={announcementImage}
+            className="announcement-image"
+            alt="announcement-1"
+          />
+          <div className="announcement-content">
+            <h3 className="announcement-title">Announcement 1</h3>
+            <p className="announcement-details">
+              Announcement details include here. Announcement details include
+              here. Announcement details include here. Announcement details
+              include here. Announcement details include here.
+            </p>
+            <div className="announcement-date">September 29, 2023</div>
+          </div>
+        </div>
+
+        <div className="announcement-card">
+          <img
+            src={announcementImage}
+            className="announcement-image"
+            alt="announcement-2"
+          />
+          <div className="announcement-content">
+            <h3 className="announcement-title">Announcement 2</h3>
+            <p className="announcement-details">
+              Announcement details include here. Announcement details include
+              here. Announcement details include here. Announcement details
+              include here. Announcement details include here.
+            </p>
+            <div className="announcement-date">September 29, 2023</div>
+          </div>
+        </div>
+
+        <div className="announcement-card">
+          <img
+            src={announcementImage}
+            className="announcement-image"
+            alt="announcement-3"
+          />
+          <div className="announcement-content">
+            <h3 className="announcement-title">Announcement 3</h3>
+            <p className="announcement-details">
+              Announcement details include here. Announcement details include
+              here. Announcement details include here. Announcement details
+              include here. Announcement details include here.
+            </p>
+            <div className="announcement-date">September 29, 2023</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Announcements;
