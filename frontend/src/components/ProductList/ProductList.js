4898e28773ce843216b7eec3c806ae8949cab89a
shanonline99 <dilshanthisaru1999@gmail.com>
2023-10-02 09:26:23 +0530

    ourProduct page

---
+++ frontend/src/components/ProductList/ProductList.js
@@ -0,0 +1,24 @@
+
+import React from 'react'
+import './ProductList.css'
+
+function ProductList({ products }) {
+  return (
+    <div className="product-list">
+      {products.map((product) => (
+        <div key={product.id} className="product-card">
+          
+          <img src={product.photo} alt={product.name} />
+          
+          <div className='product-details'>
+                <h3>{product.name}</h3>
+                <p>{product.description}</p>
+                <button>View Details</button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default ProductList;
