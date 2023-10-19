97d03faf28dc34fb495fedb0a5d0ee4ff77c8741
AnukaMithara <anukamithara@gmail.com>
2023-10-19 21:46:55 +0530

    contact us changes

--- frontend/src/pages/user/Gallery.js
+++ frontend/src/pages/user/Gallery.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react';
 import AOS from 'aos';
-import 'aos/dist/aos.css'; // You can also use <link> for styles
-// ..
+import 'aos/dist/aos.css'; 
 
 import './Gallery.css'
 import data from "../../data/index.json";
